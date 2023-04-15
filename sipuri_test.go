004ccae31665246b384141b533e7bbd07e1c0353
Alex Barter <alex.leo.barter@gmail.com>
2023-04-15 19:48:49 +0000

    Add Parse Example in tests
    This will show the example in the godocs for Parse.

--- sipuri_test.go
+++ sipuri_test.go
@@ -234,6 +234,31 @@ func TestParseError(t *testing.T) {
 	}
 }
 
+func ExampleParse() {
+	sipURI, err := sipuri.Parse("sip:user:password@host:port;uri-parameters?headers")
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the consistent components
+	fmt.Println(sipURI.User)
+	fmt.Println(sipURI.Pass)
+	fmt.Println(sipURI.Host)
+	fmt.Printf("%v\n", sipURI.Params)
+	fmt.Printf("%v\n", sipURI.Headers)
+
+	// Re-construct the URI
+	fmt.Println(sipURI.String())
+
+	// Output:
+	// user
+	// password
+	// host:port
+	// map[uri-parameters:[]]
+	// map[headers:[]]
+	// sip:user:password@host:port;uri-parameters=?headers=
+}
+
 func equalF(t *testing.T, e interface{}, g interface{}, m string, a ...interface{}) {
 	t.Helper()
 
